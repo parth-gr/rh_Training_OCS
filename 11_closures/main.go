1d8dcd1a9d1870d354cd36241ba7500237ac489c
parth-gr <partharora1010@gmail.com>
2021-01-11 14:42:46 +0530

    closures

--- 11_closures/main.go
+++ 11_closures/main.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+
+		sum += x
+		return sum
+
+	}
+}
+
 func main() {
+	sum := adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(sum(i))
+	}
 	fmt.Println("hello Parth")
 }
