83808724d08ddd4d0ca6fd177b03d0f3e4bc80e5
parth-gr <partharora1010@gmail.com>
2021-01-11 17:57:49 +0530

    structs

---
+++ 12_structs/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+//define person struct
+
+type Person struct {
+	firstname string
+	lastname  string
+	city      string
+	gender    string
+	age       int
+	// or firstname lastname string
+	//age int
+}
+
+//function pass by value,greeting method(value reciever)
+
+func (p Person) greet() string {
+	return "hello , my name is " + p.firstname + " " + p.lastname + " and I am " + strconv.Itoa(p.age)
+} //p -> identifier
+
+//hasBirthday method  (pointer reciever)
+func (p *Person) hasBirthday() {
+	p.age = 5
+}
+
+func main() {
+	//init person using struct
+
+	person1 := Person{firstname: "Parth", lastname: "arora", city: "bhopal", gender: "m", age: 25}
+	fmt.Println(person1)
+	//alternative
+	person2 := Person{"Parth", "arora", "bhopal", "m", 25}
+	person1.age = 21
+	fmt.Println(person1, person2, person1.firstname)
+
+	person1.hasBirthday()
+	fmt.Println(person1.greet())
+
+}
---
+++ 13_interfaces/main.go
@@ -0,0 +1,40 @@
+package main
+
+//datatypess which represent set of methods signatures for struct
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	area() float64
+}
+
+type Circle struct {
+	x, y, radius float64
+}
+
+type Rectangle struct {
+	width, height float64
+}
+
+func (c Circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func getArea(s Shape) float64 {
+	return s.area()
+}
+
+func main() {
+	circle := Circle{x: 0, y: 0, radius: 5}
+	rectangle := Rectangle{width: 10, height: 5}
+
+	fmt.Printf("%f\n", getArea(circle))
+	fmt.Printf("%f\n", getArea(rectangle))
+}
